1b1a46ca807745907a4bc18b9270d1ce49c7a5b9
Aqib Mehtab <aqibmehtab001@gmail.com>
2026-03-04 03:12:16 +0500

    Heavy Cpu

--- Assignments/Week 3,4,5 - Advanced-Javascript-Assignments/cpu-io/easy/heavyCPU.js
+++ Assignments/Week 3,4,5 - Advanced-Javascript-Assignments/cpu-io/easy/heavyCPU.js
@@ -8,6 +8,14 @@
 // (such as timers or async callbacks) from running until it completes.
 
 
-function heavyCPU(iterations) {}
+function heavyCPU(iterations) {
+     let sum = 0;
+
+    for (let i = 0; i < iterations; i++) {
+        sum += i; // doing some CPU work
+    }
+
+    return sum;
+}
 
 module.exports = heavyCPU;
